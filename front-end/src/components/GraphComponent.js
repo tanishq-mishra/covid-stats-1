d768ee2fee1b46b239282555846c352083024d79
Tanishq Mishra <tanishqrmishra@gmail.com>
2020-07-20 13:18:20 +0530

    Optimised Graph Component for speed

--- front-end/src/components/GraphComponent.js
+++ front-end/src/components/GraphComponent.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import ApexCharts from 'apexcharts';
 import ReactApexChart from 'react-apexcharts';
 
 let historicData = {};
+
+let confirmedDiff = [];
+let deathsDiff = [];
+let recoveredDiff = [];
+let dates = [];
+
+let displayData = [];
+let colorGraph = '';
 
 class Graph extends Component {
     constructor(props) {
@@ -14,21 +21,10 @@ class Graph extends Component {
             options: {},
             toggle: 'infected'
         }
-    }
-
-    componentWillReceiveProps(nextProps) {
-        this.setState({
-            toggle: nextProps.toggle
-        })
 
         axios.get('/graph').then(response => {
             historicData = response.data;
-
-            let confirmedDiff = [];
-            let deathsDiff = [];
-            let recoveredDiff = [];
-
-            let dates = Object.keys(historicData.cases);
+            dates = Object.keys(historicData.cases);
             dates = dates.map((date) => {
                 const dateNew = new Date(date)
                 return dateNew.toLocaleDateString('en', { day: '2-digit', month: 'short' })
@@ -43,98 +39,101 @@ class Graph extends Component {
                 deathsDiff[i] = deaths[i + 1] - deaths[i];
                 recoveredDiff[i] = recovered[i + 1] - recovered[i]
             }
+        })
+    }
 
-            let displayData = [];
-            let colorGraph = '';
+    componentWillReceiveProps(nextProps) {
 
-            if (this.state.toggle == 'infected') {
-                displayData = confirmedDiff
-                colorGraph = '#BA3131'
-            }
+        if (nextProps.toggle === 'infected') {
+            displayData = confirmedDiff
+            colorGraph = '#BA3131'
+        }
 
-            else if (this.state.toggle == 'deaths') {
-                displayData = deathsDiff
-                colorGraph = '#4E4E53'
-            }
+        else if (nextProps.toggle === 'deaths') {
+            displayData = deathsDiff
+            colorGraph = '#4E4E53'
+        }
 
-            else {
-                displayData = recoveredDiff
-                colorGraph = '#5CC1AC'
-            }
+        else {
+            displayData = recoveredDiff
+            colorGraph = '#5CC1AC'
+        }
+        this.setState({
+            toggle: nextProps.toggle,
 
-            this.setState({
-                series: [{
-                    name: 'Confirmed',
-                    data: displayData
-                }],
-                options: {
-                    chart: {
-                        height: 350,
-                        type: 'area',
-                        foreColor: '#fff',
-                        toolbar: {
-                            show: true,
-                            tools: {
-                                download: false,
-                                selection: false,
-                                zoom: false,
-                                zoomin: true,
-                                zoomout: true,
-                                pan: false,
-                                reset: false,
-                                customIcons: []
-                            },
-                            autoSelected: "selection"
+            series: [{
+                name: 'Confirmed',
+                data: displayData
+            }],
+            options: {
+                chart: {
+                    height: 350,
+                    type: 'area',
+                    foreColor: '#fff',
+                    toolbar: {
+                        show: true,
+                        tools: {
+                            download: false,
+                            selection: false,
+                            zoom: false,
+                            zoomin: true,
+                            zoomout: true,
+                            pan: false,
+                            reset: false,
+                            customIcons: []
                         },
-                    },
-                    colors: [colorGraph],
-                    grid: {
-                        show: false
-                    },
-                    dataLabels: {
-                        enabled: false
-                    },
-                    stroke: {
-                        curve: 'smooth'
-                    },
-                    tooltip: {
-                        enabled: false
-                    },
-                    fill: {
-                        colors: undefined,
-                        opacity: 0.2,
-                        type: 'gradient',
-                        gradient: {
-                            shade: 'dark',
-                            type: "vertical",
-                            shadeIntensity: 0.5,
-                            gradientToColors: undefined,
-                            inverseColors: true,
-                            opacityFrom: 0.7,
-                            opacityTo: 0.3,
-                        }
-                    },
-                    xaxis: {
-                        rotate: '90',
-                        type: 'Date',
-                        categories: dates,
-                        labels: {
-                            rotate: 0,
-                        },
-                        tickAmount: 3,
-                        tickPlacement: 'on',
-                        axisTicks: {
-                            show: true,
-                            borderType: 'solid',
-                            color: '#78909C',
-                            height: 6,
-                            offsetX: 0,
-                            offsetY: 0
-                        },
+                        autoSelected: "selection"
                     },
                 },
-            })
+                colors: [colorGraph],
+                grid: {
+                    show: false
+                },
+                dataLabels: {
+                    enabled: false
+                },
+                stroke: {
+                    curve: 'smooth'
+                },
+                tooltip: {
+                    enabled: false
+                },
+                fill: {
+                    colors: undefined,
+                    opacity: 0.2,
+                    type: 'gradient',
+                    gradient: {
+                        shade: 'dark',
+                        type: "vertical",
+                        shadeIntensity: 0.5,
+                        gradientToColors: undefined,
+                        inverseColors: true,
+                        opacityFrom: 0.7,
+                        opacityTo: 0.3,
+                    }
+                },
+                xaxis: {
+                    rotate: '90',
+                    type: 'Date',
+                    categories: dates,
+                    labels: {
+                        rotate: 0,
+                    },
+                    tickAmount: 3,
+                    tickPlacement: 'on',
+                    axisTicks: {
+                        show: true,
+                        borderType: 'solid',
+                        color: '#78909C',
+                        height: 6,
+                        offsetX: 0,
+                        offsetY: 0
+                    },
+                },
+            },
         })
+
+
     }
 
     render() {
